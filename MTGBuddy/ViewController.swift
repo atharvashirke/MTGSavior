68defb53bcd24d2bdecfef3c3a9b130da618b00e
Atharva Shirke <atharvashirke@berkeley.edu>
2019-10-28 05:55:23 -0700

    Initial Commit

---
+++ MTGBuddy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MTGBuddy
+//
+//  Created by Atharva Shirke on 10/28/19.
+//  Copyright © 2019 Atharva Shirke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
