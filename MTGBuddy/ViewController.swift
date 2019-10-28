80f4f2c5f88dbd794898dfd2b8dfd3f90031264e
Atharva Shirke <atharvashirke@berkeley.edu>
2019-10-28 08:56:02 -0700

    Completed life counter system and fixed alignment issues.

--- MTGBuddy/ViewController.swift
+++ MTGBuddy/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var OpponentLifeCount: UILabel!
+    @IBOutlet weak var OpponentLifeIncrement: UIButton!
+    @IBOutlet weak var OpponentLifeDecrement: UIButton!
+    @IBOutlet weak var YourLifeCount: UILabel!
+    @IBOutlet weak var YourLifeDecrement: UIButton!
+    @IBOutlet weak var YourLifeIncrement: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func OpponentLifeIncrementPressed(_ sender: Any) {
+        let lastValue: Int = Int(OpponentLifeCount.text ?? "") ?? 0
+        OpponentLifeCount.text = "\(lastValue + 1)"
+    }
+    
+    @IBAction func OpponentLifeDecrementPressed(_ sender: Any) {
+        let lastValue: Int = Int(OpponentLifeCount.text ?? "") ?? 0
+        OpponentLifeCount.text = "\(lastValue - 1)"
+    }
+    
+    @IBAction func YouLifeDecrement(_ sender: Any) {
+        let lastValue: Int = Int(YourLifeCount.text ?? "") ?? 0
+        YourLifeCount.text = "\(lastValue - 1)"
+    }
+    
+    @IBAction func YourLifeIncrement(_ sender: Any) {
+        let lastValue: Int = Int(YourLifeCount.text ?? "") ?? 0
+        YourLifeCount.text = "\(lastValue + 1)"
+    }
+    
 
 }
 
